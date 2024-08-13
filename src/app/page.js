4b4540af396e05895c25fd0b7a62be6337f53166
marleymwangi <bobmwangee@gmail.com>
2024-08-13 08:41:31 +0300

    mobile ban

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,46 @@
-'use client';
-import styles from './page.module.scss'
-import { useEffect, useState } from 'react'
-import { AnimatePresence } from 'framer-motion';
-import Preloader from '../components/Preloader';
-import Landing from '../components/Landing';
-import Projects from '../components/Projects';
-import Description from '../components/Description';
-import SlidingImages from '../components/SlidingImages';
-import Contact from '../components/Contact';
+"use client";
+import styles from "./page.module.scss";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import Preloader from "../components/Preloader";
+import Landing from "../components/Landing";
+import Projects from "../components/Projects";
+import Description from "../components/Description";
+import SlidingImages from "../components/SlidingImages";
+import Contact from "../components/Contact";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { TextEffect } from "./animation";
 
 export default function Home() {
-
+  const isDesktop = useMediaQuery("(max-width: 768px)");
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect( () => {
-    (
-      async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll();
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
 
-          setTimeout( () => {
-            setIsLoading(false);
-            document.body.style.cursor = 'default'
-            window.scrollTo(0,0);
-          }, 2000)
-      }
-    )()
-  }, [])
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
+  if (isDesktop) {
+    return (
+      <main className={styles.mobile}>
+        <TextEffect per="char" preset="fade">
+          For the best experience, please use a desktop device.
+        </TextEffect>
+      </main>
+    );
+  }
 
   return (
     <main className={styles.main}>
-      <AnimatePresence mode='wait'>
+      <AnimatePresence mode="wait">
         {isLoading && <Preloader />}
       </AnimatePresence>
       <Landing />
@@ -39,5 +49,5 @@ export default function Home() {
       <SlidingImages />
       <Contact />
     </main>
-  )
+  );
 }
